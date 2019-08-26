1c5920d4c369432c254872eeb54ecad7441b33da
abarnola <Alebarnola96@gmail.com>
2019-08-26 18:18:47 -0400

    Add color palette, navbar, routing
    Get Tells from backend with axios, show them on homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+//Components
+import NavBar from './components/Navbar'
+
+//Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#F2D7EE',
+      main: '#A5668B',
+      dark: '#69306D',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#6a1b9a',
+    }
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <h1>TELL 'EM</h1>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+        <NavBar />
+          <div className="container"> 
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+
+export class home extends Component {
+    state = {
+        tells: null
+    }
+    componentDidMount() {
+        axios.get('/tells')
+            .then(res => {
+                this.setState({
+                    tells: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        let recentTellsMarkup = this.state.tells ? (
+            this.state.tells.map(tell => {
+                return <p>{tell.body}</p>
+            })
+        ) : <p>Loading...</p>
+        return (
+            <Grid container spacing={16} >
+                 <Grid item sm={8} xs={12}>
+                     {recentTellsMarkup}
+                 </Grid>
+                 <Grid item sm={4} xs={12}>
+                     <p>Profile info here</p>
+                 </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home
